e87e43bc242396071c462707f90fa20545949ace
Jordan Zhang <chrn.zhng@gmail.com>
2017-11-13 11:07:18 -0800

    learned about v-spacer, added some more flat buttons to toolbar.

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -3,7 +3,20 @@
      <v-toolbar-title class="mr-4">
          TabTracker
      </v-toolbar-title>
+    <!-- TODO: Implement Me -->
+     <!-- <v-toolbar-items>
+         <v-btn flat dark>
+             Browse
+         </v-btn>
+     </v-toolbar-items> -->
+     <v-spacer></v-spacer>
+     <v-toolbar-items>
+         <v-btn flat dark>
+             Sign Up
+         </v-btn>
+     </v-toolbar-items>
  </v-toolbar>
+
 </template>
 
 <script>
