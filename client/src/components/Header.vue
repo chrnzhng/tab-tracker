151f0c6c26b6d4252b00eaab101098b6500796dd
Jordan Zhang <chrn.zhng@gmail.com>
2017-11-16 10:27:13 -0800

    set up a login page

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -18,6 +18,12 @@
      <v-spacer></v-spacer>
      <v-toolbar-items>
          <v-btn flat dark
+         @click="navigateTo({name: 'login'})">
+            Login
+         </v-btn>    
+     </v-toolbar-items>
+     <v-toolbar-items>
+         <v-btn flat dark
          @click="navigateTo({name: 'register'})">
             Sign Up
          </v-btn>    
